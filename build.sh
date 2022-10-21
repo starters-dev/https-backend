023136aaa195e4466b3e55a0dcf977751507b387
Batyr Kanzitdinov <dev@batyr.io>
2022-10-22 01:45:28 +0200

    `build` script

---
+++ build.sh
@@ -0,0 +1,14 @@
+# -- running setup script
+bash run/setup.sh
+
+# -- getting all docker-compose files
+DC_FILES="docker-compose.main.yml"
+for F in `find ./services -type f -maxdepth 1 -name "docker-compose*.yml"`
+do
+  DC_FILES="$DC_FILES -f $F "
+done
+
+# -- building containers
+# docker-compose $DC_FILES build && docker-compose $DC_FILES up -d
+
+echo $DC_FILES
