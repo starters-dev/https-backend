62485fc07af9fe0a4948575b5ad3df82e93d6194
Batyr Kanzitdinov <dev@batyr.io>
2022-10-23 20:01:47 +0200

    imprv

--- services/plausible/verify_emails.sh
+++
@@ -1,2 +0,0 @@
-# as stated here -- https://plausible.io/docs/self-hosting#3-start-the-server
-docker-compose exec plausible_db psql -U postgres -d plausible_db -c "UPDATE users SET email_verified = true;"
